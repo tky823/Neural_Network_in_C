6a537b7c33f192a2e28023b57386a14cc06e47a8
tky1117 <40362510+tky1117@users.noreply.github.com>
2019-08-07 22:14:04 +0900

    Add activation function example

---
+++ activation_function_example/_activation_function.h
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <math.h>
+
+float *step(float *a, int D);
+float *ReLU(float *a, int D);
+float *sigmoid(float *a, int D);
+float *softmax(float *a, int K);
+
+float *step(float *a, int D) {
+    int d;
+    float *z = NULL;
+    
+    if((z = (float *)malloc((D + 1) * sizeof(float))) == NULL) {
+        return NULL;
+    }
+    
+    for(d = 1; d <= D; d++) {
+        if(a[d] < 0.0f) {
+            z[d] = 0.0f;
+        } else {
+            z[d] = 1.0f;
+        }
+    }
+    
+    return z;
+}
+
+float *ReLU(float *a, int D) {
+    int d;
+    float *z = NULL;
+    
+    if((z = (float *)malloc((D + 1) * sizeof(float))) == NULL) {
+        return NULL;
+    }
+    
+    for(d = 1; d <= D; d++) {
+        if(a[d] < 0.0f) {
+            z[d] = 0.0f;
+        } else {
+            z[d] = a[d];
+        }
+    }
+    
+    return z;
+}
+
+float *sigmoid(float *a, int D) {
+    int d;
+    float *z = NULL;
+    
+    if((z = (float *)malloc((D + 1) * sizeof(float))) == NULL) {
+        return NULL;
+    }
+    
+    for(d = 1; d <= D; d++) {
+        z[d] = 1.0f / (1.0f + expf(- a[d]));
+    }
+    
+    return z;
+}
+
+float *softmax(float *a, int K) {
+    int k;
+    float *y = NULL;
+    float sum_exp = 0.0f;
+    
+    if((y = (float *)malloc((K + 1) * sizeof(float))) == NULL) {
+        return NULL;
+    }
+
+    for(k = 1; k <= K; k++) {
+        y[k] = expf(a[k]);
+        sum_exp += y[k];
+    }
+    
+    for(k = 1; k <= K; k++) {
+        y[k] /= sum_exp;
+    }
+    
+    return y;
+}
