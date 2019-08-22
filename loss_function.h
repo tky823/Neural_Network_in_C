e6a22ef9807846a82c835237f82cf98438021d69
tky1117 <40362510+tky1117@users.noreply.github.com>
2019-08-22 17:58:28 +0900

    add functions

---
+++ loss_function.h
@@ -0,0 +1,42 @@
+#include <math.h>
+
+float mean_squared_error(float *y, float *t, int D, int derivative, float *dE_dy);
+float cross_entropy(float *y, float *t, int K, int derivative, float *dE_dy);
+
+float mean_squared_error(float *y, float *t, int D, int derivative, float *dE_dy) {
+    int d;
+    float delta;
+    float E = 0.0f;
+
+    if(derivative) {
+        for(d = 1; d <= D; d++) {
+            dE_dy[d] = - 2.0f * (t[d] - y[d]) / (float) D;
+        }
+        return 0.0f;
+    }
+
+    for(d = 1; d <= D; d++) {
+        delta = t[d] - y[d];
+        E += delta * delta;
+    }
+    
+    return E / (float) D;
+}
+
+float cross_entropy(float *y, float *t, int K, int derivative, float *dE_dy) {
+    int k;
+    float E = 0.0f;
+    
+    if(derivative) {
+        for(k = 1; k <= K; k++) {
+            dE_dy[k] = - t[k] / y[k];
+        }
+        return 0.0f;
+    }
+    
+    for(k = 1; k <= K; k++) {
+        E -= t[k] * logf(y[k]);
+    }
+    
+    return E;
+}
